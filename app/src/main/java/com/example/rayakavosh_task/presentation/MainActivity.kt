294a7b9bd225fd13c183cd8fb4d8501b54877692
payam <payam.elm@gmail.com>
2021-01-07 23:37:59 +0330

    First Commit Setting up Architecture and Gradle files

---
+++ app/src/main/java/com/example/rayakavosh_task/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.rayakavosh_task.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.rayakavosh_task.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
